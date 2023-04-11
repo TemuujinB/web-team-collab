06cd902a5dfbfdadf16557a73117a1a6ec5d288b
TemuujinB <124540561+TemuujinB@users.noreply.github.com>
2023-04-11 10:46:54 +0800

    json, js

---
+++ js/console.js
@@ -0,0 +1,20 @@
+fetch('./json/console.json')
+    .then(response => response.json())
+    .then(data => {
+        const articleElements = document.getElementsByTagName('article');
+
+        for (let i = 0; i < data.articles.length; i++) {
+            const articleElement = articleElements[i];
+            const article = data.articles[i];
+            articleElement.innerHTML = `
+                <h2>${article.title}</h2>
+                <h3>${article.subtitlee}</h3>
+                <div>
+                    <a href="${article.buyLink.href}">${article.buyLink.text}</a>
+                    <a href="${article.learnMoreLink.href}">${article.learnMoreLink.text}</a>
+                </div>
+            `;
+            articleElement.setAttribute('style', article.style);
+        }
+    })
+    .catch(error => console.error('Error: ', error));
