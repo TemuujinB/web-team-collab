fd69da27441c5904faec758799a5472697a21415
amgaland <120997955+amgaland@users.noreply.github.com>
2023-05-23 18:55:19 +0800

    update

--- Buy section/js for buy section/component-card.js
+++ Buy section/js for buy section/component-card.js
@@ -4,8 +4,52 @@ class ComponentCard extends HTMLElement {
         //implementation
     }
 
+    render(product){
+        this.innerHTML=`
+        <div class="border ">
+            <img src="${product.image}" alt="" style="width: 150px; height: 150px; float: left;" class="mr-4">
+            <h2>${product.name}</h2>
+            <h2>${product.price}</h2s>
+        </div>`
+    }
+
     connectedCallback() {
-        localStorage.getItem("myProducts");
+        const myProducts = localStorage.getItem("myProducts");
+        console.log(myProducts);
+        const list = JSON.parse(myProducts);
+        const idlist = list.map(i=>i.queryString);
+        console.log(idlist);
+        const str = idlist.toString();
+
+        const regexp = /"([^"]*)"/;
+        const match = str.match(regexp);
+        console.log(match)
+        // const resultId = match[1];
+
+        // fetch('https://api.jsonbin.io/v3/b/64341dbaebd26539d0a83299')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         const str = this.id;
+        //         const str2 = this.type;
+        //         const regexp = /"([^"]*)"/;
+        //         const match = str.match(regexp);
+        //         const match2 = str2.match(regexp);
+        //         const resultId = match[1];
+        //         const resultType = match2[1];
+        //         console.log("id: ",resultId);
+        //         console.log("type: ",resultType);
+        //         console.log(data);
+        //         data.map(dt=>{
+        //             if(dt.type == resultId){
+        //                 console.log(dt.type);
+        //                 this.#Render(dt)
+        //             }
+        //         })
+        //     })
+        //     .catch(error => {
+        //         // Handle any errors that occur during the request
+        //         console.error('Error:', error);
+        //     });
     }
 
     disconnectedCallback() {
@@ -19,6 +63,7 @@ class ComponentCard extends HTMLElement {
     adoptedCallback() {
         //implementation
     }
+
 
 }
 
--- server/app.js
+++ server/app.js
@@ -1,45 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const swaggerJSDocs = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+const express = require('express');
+const cors = require('cors');
+const swaggerJSDocs = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const app = express();
+const indexRouter = require('./router/router.js');
+const PORT = 4000;
 
-const app = express()
-const indexRouter= require('./router/router.js')
-const PORT = 4000
-
-// middleware
-app.use(express())
-
-app.use(cors())
+// Middleware
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
+app.use(cors());
 
 const options = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Comsco api',
-        version: '1.0.0',
-      },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Comsco API',
+      version: '1.0.0',
     },
-    apis: ['app.js', './router/*.js'], // files containing annotations as above
+  },
+  apis: ['app.js', './router/*.js'], // Files containing annotations
 };
-    
+
 const openapiSpec = swaggerJSDocs(options);
-    
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup( openapiSpec));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.use((req, res, next) => {
-    console.log(req.url, req.method);
-    next()    
-})  
+  console.log(req.url, req.method);
+  next();
+});
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`);
-}) 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-
-// import indexRouter from './routes/index.js'
-
-
-
-app.use('/api/', indexRouter)
+app.use('/api/', indexRouter);
