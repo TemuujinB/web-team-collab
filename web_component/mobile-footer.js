356498689f6798e1448e2dab22169e16061f58f9
Solongohh <soko7596@gmail.com>
2023-05-28 15:21:41 +0800

    sok

--- web_component/mobile-footer.js
+++ web_component/mobile-footer.js
@@ -1,28 +1,32 @@
 class MobileFooter extends HTMLElement {
     constructor() {
         super();
+        // this.attachShadow({mode:"open"})
         this.render();
     }
     render(){
         this.innerHTML = `
         <style>
-        footer{
+        mobile-footer{
             width: 100%;
-            height: 10vh;
-            background-color: white;
             display: flex;
-            flex-direction:row;
             justify-content: space-around;
             align-items: center;
+            height: 50px;
         }
-        #Layer_1{
-            width: 25px;
+        a img{
+            box-shadow:    0 0 .2rem #fff,
+            0 0 .2rem #fff,
+            0 0 2rem #bc13fe,
+            0 0 0.8rem #bc13fe,
+            0 0 2.8rem #bc13fe,
+            inset 0 0 1.3rem #bc13fe; 
         }
         </style>
-        <a href="" alt=""><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/home--v2.png" alt="home--v2"/></a>
-        <img width="24" height="24" src="https://img.icons8.com/external-outline-agus-raharjo/64/external-playstation-electronic-outline-agus-raharjo.png" alt="external-playstation-electronic-outline-agus-raharjo"/>
-        <img width="24" height="24" src="https://img.icons8.com/external-those-icons-fill-those-icons/24/external-headphone-music-audio-those-icons-fill-those-icons-1.png" alt="external-headphone-music-audio-those-icons-fill-those-icons-1"/>
-        <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/controller.png" alt="controller"/>`
+        <a href="../index.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/home--v2.png" alt="home--v2"/></a>
+        <a href="../pages/console.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/external-outline-agus-raharjo/64/external-playstation-electronic-outline-agus-raharjo.png" alt="external-playstation-electronic-outline-agus-raharjo"/></a>
+        <a href="../pages/accessories.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/external-those-icons-fill-those-icons/24/external-headphone-music-audio-those-icons-fill-those-icons-1.png" alt="external-headphone-music-audio-those-icons-fill-those-icons-1"/></a>
+        <a href="../pages/games.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/controller.png" alt="controller"/></a>`
     }
     connectedCallback() {
         
--- web_component/mobile-header.js
+++ web_component/mobile-header.js
@@ -12,9 +12,11 @@ class MobileHeader extends HTMLElement {
         header{
             display:flex;
             justify-content:space-around;
+            align-items: center;
         }
         .mobileHeader{
             width: 100%;
+            height: auto;
             display: flex;
             flex-direction: row;
             align-items:center;
@@ -22,12 +24,15 @@ class MobileHeader extends HTMLElement {
         .mobileHeader img{
             width: 35px;
         }
+        nav ul{
+            margin-left: 20px;
+            margin-right: 20px;
+        }
         </style>
         <nav class="mobileHeader">
             <ul><img src="../img/logo.png" alt="logo"></ul>
-            <ul><a href="index.html">COMSCOPE</a></ul>
-            <ul><i class="fa-solid fa-cart-shopping"></i></ul>
-            <ul><shopping-cart></shopping-cart></ul>
+            <ul><a href="../pages/store.html">COMSCOPE</a></ul>
+            <ul><a href="../Buy section/Cart2.html" alt="shopping"><i class="fa-solid fa-cart-shopping"></i></a><shopping-cart></shopping-cart></ul>
         </nav>
         `
     }
